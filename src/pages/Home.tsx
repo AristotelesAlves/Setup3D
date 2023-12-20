81d4f5def4f87488267cbd144d3ccfe547fbf458
aristotelesalves <arystotelys@gmail.com>
2023-12-20 12:56:01 -0300

    responsividade

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,23 +4,23 @@ const Spline = React.lazy(() => import('@splinetool/react-spline'));
 
 export function Home() {
   return (
-    <div className="w-screen h-screen p-5 bg-zinc-800 flex justify-center gap-5 items-center flex-col">
-      <h1 className='font-bold text-xl text-white absolute top-6'>
+    <div className="w-full h-full max-sm:h-screen py-5 bg-zinc-800 flex justify-center gap-5 items-center flex-col">
+      <h1 className='font-bold text-2xl w-full text-center max-sm:text-xl text-white'>
           Bem-vindo ao meu universo digital em 3D! 🚀
-        </h1>
+      </h1>
       <Suspense fallback={
-          <div className='w-[80%] h-[80%]'>
+          <div className='max-sm:h-full max-sm:w-full h-full'>
             Loading...
           </div>
         }>
-        <div className='w-[80%] h-[80%]'>
+        <div className='max-sm:h-full max-sm:w-full h-full'>
           <Spline scene="https://prod.spline.design/QTx4tecVaGWmBWnT/scene.splinecode" />
         </div>
       </Suspense>
       <div className=''>
         
        
-        <div className='text-white mt-3 flex flex-col gap-1 text-sm w-full text-center -mt-10'>
+        <div className='text-white mt-3 flex flex-col gap-1 text-sm w-full text-center -mt-10 px-1'>
           <p className='font-bold text-base text-white text-center'>
             Aqui estão os destaques do meu setup:
           </p>
